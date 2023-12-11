3dbd43a98e8361039e0d3c71526bbb4615d73869
MiralisMH <kylian.riberou@etu.univ-orleans.fr>
2023-12-11 18:25:31 +0100

    feature: creation classe Message

---
+++ Application/reseausocial/src/main/java/reseausocial/Message.java
@@ -0,0 +1,9 @@
+package reseausocial;
+
+public class Message {
+    //TODO: 
+    // - Date
+    // - Contenu
+    // - Auteur  ? 
+    // - Destinataire ? tout dépend des consignes si on envoie en broadcast ou pas
+}
--- Application/reseausocial/src/main/java/reseausocial/Utilisateur.java
+++ Application/reseausocial/src/main/java/reseausocial/Utilisateur.java
@@ -1,10 +1,14 @@
 package reseausocial;
 
+import java.util.List;
+
 import lombok.*;
 
 @Getter
 @Setter
 public class Utilisateur {
     private String nom;
-    
+
+    private List<Utilisateur> abonnes;
+    //TODO : Liste de messages, classe Message obligée pour leur donner une date, tout ca ...
 }
