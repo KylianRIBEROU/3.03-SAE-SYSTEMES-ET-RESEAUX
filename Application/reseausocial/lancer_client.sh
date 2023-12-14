7279c9575290e5e72e4cac07c06ecbd04f97b038
MiralisMH <kylian.riberou@etu.univ-orleans.fr>
2023-12-14 11:43:36 +0100

    feature: scripts lancement OK. Relation client-serveur init

--- Application/reseausocial/lancer_client.sh
+++ Application/reseausocial/lancer_client.sh
@@ -4,11 +4,13 @@
 # on verra pour demander un input à l'utilisateur si il n'a rien mis en paramètres d'entrée
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied"
-    mvn clean install exec:java@run-client -Dexec.args="localhost date" -DSkipTests
+    echo "Vous devez entrer 2 arguments. Le Hostname et votre nom d'utilisateur"
+    echo "Nom du serveur"
+    read -p "> " arg1
+    echo "Nom d'utilisateur"
+    read -p "> " arg2
+    mvn clean install exec:java@run-client -Dexec.args="$arg1 $arg2" -DskipTests=true
   else
     echo "Arguments supplied"
-    mvn clean install exec:java@run-client -Dexec.args="$1 $2" -DSkipTests
+    mvn clean install exec:java@run-client -Dexec.args="$1 $2" -DskipTests=true
 fi
-```
-
--- Application/reseausocial/lancer_serveur.sh
+++ Application/reseausocial/lancer_serveur.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn clean install exec:java@run-server -DSkipTests
+mvn clean install exec:java@run-server -DskipTests=true
